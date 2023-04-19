fb0b3a594d7e5a14c3fb04c2b3e00b1a350726a7
alefmiguel <alefmiguel45@gmail.com>
2023-04-19 10:03:45 -0300

    adicionando tarefas e ideia de flatlist

--- src/components/NotePad/NotePad.jsx
+++ src/components/NotePad/NotePad.jsx
@@ -1,18 +1,33 @@
 import styles from './NotePad.module.css';
 import { MdOutlineAddCircle } from 'react-icons/md';
 import Notes from '../Notes/Notes';
+import { useState } from 'react';
 
 function NotePad() {
+    const [components, setComponents] = useState([]);
+
+    function handleAddNote() {
+        const newComp = <Notes />
+        setComponents([...components, newComp])
+    }
+
+    function handleDeleteNote() {
+    }
+
     return ( 
         <div className={styles.container}>  
             <div className={styles.posicionarTextos}>
                 <h2> Adicione Uma Tarefa </h2>
-                <span> <MdOutlineAddCircle /> </span>
+                <span onClick={handleAddNote}> <MdOutlineAddCircle /> </span>
             </div>
 
-            <section className="notas">
-                <Notes />
-            </section>
+            <div className="notas">
+                {components.map((component, index) => (
+                    <section key={index}>
+                        {component}
+                    </section>
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,11 +1,29 @@
 import styles from './Notes.module.css'
-import { RiDeleteBin2Fill } from 'react-icons/ri'
+import { RiDeleteBin2Fill, RiSettings2Fill } from 'react-icons/ri'
 import { FaEdit } from 'react-icons/fa'
+import { useState } from 'react';
 
 function Notes() {
+    const [doneNote, setDoneNote] = useState(false);
+
+    function handleClick() {
+        setDoneNote(!doneNote);
+    }
+
+    function handleDeleteDiv() {
+        const campo = document.querySelector('#peidei').style.display = 'none'
+        alert(campo.style.display)
+    }
+
     return (
-        <div className={styles.container}>
-           <div className={styles.doneNote}> a </div>
+        <div onClick={handleClick} className={styles.container}>
+           {doneNote && 
+            <div className={styles.doneNoteIn}> ⠀ </div>
+           }
+           
+           {!doneNote &&
+            <div id="peidei" onAnimationEnd={handleDeleteDiv} className={styles.doneNoteOut}> ⠀ </div>
+            }
 
            <section className={styles.spacingNote}>
             <h3 className={styles.noteName}>
